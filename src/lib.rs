2d8d4a1b928c9fdddc404c9a11b6ed8d212610c7
Thimo Neumann <tiquthon@gmail.com>
2020-11-30 19:06:17 +0100

    Remove Code Bullet crate and add Tiquthon crate

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,10 @@
 //! For a quick start and most other use cases it's sufficient to include this as a single
 //! dependency.
 
+pub extern crate gymnarium_agents_random;
+// pub extern crate gymnarium_agents_q_learning;
 pub extern crate gymnarium_base;
 pub extern crate gymnarium_environments_gym;
+pub extern crate gymnarium_environments_tiquthon;
 pub extern crate gymnarium_visualisers_base;
 pub extern crate gymnarium_visualisers_piston;
-// pub extern crate gymnarium_environments_tiquthon;
-pub extern crate gymnarium_agents_random;
-pub extern crate gymnarium_environments_code_bullet;
-// pub extern crate gymnarium_agents_q_learning;
